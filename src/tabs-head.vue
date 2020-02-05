280fbf4a4b517bb3aa11bc1b1de7cbaa000d2682
XiaoBaoJun <xbj1525@126.com>
2020-02-05 14:26:40 +0800

    完善tabs的样式

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -40,12 +40,13 @@ export default {
 <style lang='scss' scoped>
 $tab-height: 40px;
 $blue: blue;
+$border-color:#ddd;
 .tabs-head {
   display: flex;
   height: $tab-height;
   justify-content: flex-start;
-  align-items: center;
   position: relative;
+  border-bottom: 1px solid $border-color;
   > .line {
     position: absolute;
     bottom: 0;
@@ -54,6 +55,10 @@ $blue: blue;
   }
   > .actions-wrapper {
     margin-left: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 1em;
   }
 }
 </style>
